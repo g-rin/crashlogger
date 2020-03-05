722bf5441756f63cee7919fa248f4e0f1ff7479a
Ринат Гадельшин <gadelshin.r.n@topaz-atcs.com>
2020-03-05 17:58:09 +0300

    refactoring

--- include/crashlogger.h
+++ include/crashlogger.h
@@ -1,6 +1,9 @@
 #ifndef CRASHLOGGER_H
 #define CRASHLOGGER_H
 
-void initCrashlogger();
+void initCrashLogger( const char* report_filename );
+void releaseCrashLogger();
+
+void reinitSignalHandlers();
 
 #endif  // CRASHLOGGER_H
--- src/crashlogger.cpp
+++ src/crashlogger.cpp
@@ -1,9 +1,20 @@
 #include "backtrace.h"
 #include "backtrace-supported.h"
 #include <cxxabi.h>
+#include <errno.h>
+#include <execinfo.h>
+#include <inttypes.h>
+#include <setjmp.h>
 #include <signal.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
 #include <unistd.h>
-#include <qglobal.h>
+#include <QFileInfo>
+#include <QDir>
+
 #ifndef PRIxPTR
 #    if __WORDSIZE == 64
 #        define PRIxPTR "lx"
@@ -12,35 +23,82 @@
 #    endif
 #endif
 
-static bool printBacktrace;
-static bool useAnsiColor;
-static bool dumpCore;
+static void sigHandler( int sig ) __attribute((noreturn));
+static void writeCrashLog( const char* why, const int stackFramesToIgnore );
 
-static char *demangleBuffer;
+static FILE* reportFile = nullptr;
+static char* demangleBuffer;
 static size_t demangleBufferSize;
+static char SIGSEGV_BUFFER[ SIGSTKSZ ];
+static char SIGABRT_BUFFER[ SIGSTKSZ ];
+static char SIGFPE_BUFFER[ SIGSTKSZ ];
+static char SIGPIPE_BUFFER[ SIGSTKSZ ];
+static char LOGBUFFER[ SIGSTKSZ ];
 
-static int stackFramesLimit = 100;
+static struct timeval TIME_VAL;
+static struct tm TIMESTAMP;
+static char TIMESTAMP_BUFFER[64];
 
-static char SIGSEGV_BUFFER[SIGSTKSZ];
-static char SIGABRT_BUFFER[SIGSTKSZ];
-static char SIGFPE_BUFFER[SIGSTKSZ];
+#pragma GCC diagnostic ignored "-Wformat-nonliteral"
+static void printToLog( const char* format, ...) {
+   static va_list arguments;
+   static int length;
+   va_start(arguments, format);
+   length = vsprintf(LOGBUFFER, format, arguments);
+   va_end(arguments);
+   LOGBUFFER[ length ] = '\0';
+   fprintf( stderr, "%s", LOGBUFFER );
+   if ( reportFile ) {
+       fprintf( reportFile, "%s", LOGBUFFER );
+   }
+}
+#pragma GCC diagnostic warning "-Wformat-nonliteral"
 
-static void writeCrashLog(const char *why, int stackFramesToIgnore) __attribute__((noreturn));
-
-static void sigHandler(int sig)
-{
-    // this lambda is the low-level signal handler multiplexer
-    // resetToDefault(sig);
-    static char buffer[256];
-    snprintf(buffer, sizeof(buffer), "uncaught signal %d (%s)", sig, sys_siglist[sig]);
-    // 3 means to remove 3 stack frames: this way the backtrace starts at the point where
-    // the signal reception interrupted the normal program flow
-    writeCrashLog(buffer, 3);
+static void printCurrentTime() {
+    gettimeofday( &TIME_VAL, nullptr );
+    gmtime_r( &TIME_VAL.tv_sec, &TIMESTAMP );
+    strftime( TIMESTAMP_BUFFER, sizeof( TIMESTAMP_BUFFER ), "[%Y-%m-%d %H:%M:%S UTC] ", &TIMESTAMP );
+    printToLog( "%s", TIMESTAMP_BUFFER );
 }
 
-static void install(int sig, char*const buffer)
-{
-#if defined(Q_OS_UNIX)
+void releaseCrashLogger() {
+    fflush( stderr );
+
+    if ( not reportFile ) {
+        return;
+    }
+
+    fflush( reportFile );
+    if ( 0 != fclose( reportFile ) ) {
+        const int error_code = errno;
+        printToLog( "Could not close crashlogger's file. Cause: %s\n", strerror( error_code ) );
+    }
+    reportFile = nullptr;
+
+    signal(SIGSEGV, SIG_DFL);
+    signal(SIGABRT, SIG_DFL);
+    signal(SIGFPE, SIG_DFL);
+    signal(SIGPIPE, SIG_DFL);
+}
+
+static void sigHandler( int sig ) {
+    // this lambda is the low-level signal handler multiplexer
+    // resetToDefault(sig);
+    static char buffer[ 256 ];
+    snprintf( buffer, sizeof( buffer ), "uncaught signal %d (%s)", sig, sys_siglist[ sig ] );
+
+    // 3 means to remove 3 stack frames: this way the backtrace starts at the point where
+    // the signal reception interrupted the normal program flow
+    writeCrashLog( buffer, 3 );
+    releaseCrashLogger();
+    abort();
+}
+
+static void sigPipeHandler( int ) {
+    writeCrashLog( "SIGPIPE has been received!", 3 );
+}
+
+static void install( int sig, char*const buffer ) {
     // Use alternate signal stack to get backtrace for stack overflow
     stack_t sigstack;
     sigstack.ss_sp = buffer;
@@ -60,13 +118,26 @@ static void install(int sig, char*const buffer)
     sigaction(sig, &sigact, nullptr);
 
     sigprocmask(SIG_UNBLOCK, &unblockSet, nullptr);
-#else
-    signal(sig, sigHandler);
-#endif
 }
 
-void initCrashlogger()
-{
+void reinitSignalHandlers() {
+    install( SIGSEGV, SIGSEGV_BUFFER );
+    install( SIGABRT, SIGABRT_BUFFER );
+    install( SIGFPE, SIGFPE_BUFFER );
+
+    stack_t sigpipestack;
+    sigpipestack.ss_sp = SIGPIPE_BUFFER;
+    sigpipestack.ss_size = SIGSTKSZ;
+    sigpipestack.ss_flags = 0;
+    sigaltstack(&sigpipestack, nullptr);
+    struct sigaction sigpipe_action;
+    sigpipe_action.sa_flags = SA_ONSTACK;
+    sigpipe_action.sa_handler = sigPipeHandler;
+    sigemptyset(&sigpipe_action.sa_mask);
+    sigaction(SIGPIPE, &sigpipe_action, nullptr);
+}
+
+void initCrashLogger(  const char* report_filename ) {
     // This can catch and pretty-print all of the following:
 
     // SIGFPE
@@ -83,175 +154,189 @@ void initCrashlogger()
     // uncaught std::exception derived exception (prints what())
     // throw std::logic_error("test output");
 
-    printBacktrace = true;
-    dumpCore = true;
+    QFileInfo finfo( report_filename );
+    auto logdir = finfo.dir();
+    if ( not logdir.exists() ) {
+        if ( not logdir.mkpath( logdir.path() ) ) {
+            const int error_code = errno;
+            printToLog( "Could create logdir '%s' for the crashlog. Cause: %s\n",
+                        qPrintable( logdir.path() ),
+                        strerror( error_code ) );
+            return;
+        }
+    }
 
-#if defined(Q_OS_UNIX)
-    if (::isatty(STDERR_FILENO))
-        useAnsiColor = true;
-#else
-    useAnsiColor = false;
-#endif
+    reportFile = fopen( report_filename, "wa" );
+    if ( not reportFile ) {
+        const int error_code = errno;
+        printToLog( "Could not open file '%s'. Cause: %s\n", report_filename, strerror( error_code ) );
+        return;
+    }
 
     demangleBufferSize = 512;
-    demangleBuffer = (char *)malloc(demangleBufferSize);
+    demangleBuffer = reinterpret_cast< char* >( malloc( demangleBufferSize ) );
 
-    // handling signals
-    //{ SIGFPE, SIGSEGV, SIGILL, SIGBUS, SIGPIPE, SIGABRT },
-    install(SIGSEGV, SIGSEGV_BUFFER );
-    install(SIGABRT, SIGABRT_BUFFER);
-    install(SIGFPE, SIGFPE_BUFFER);
+    install( SIGSEGV, SIGSEGV_BUFFER );
+    install( SIGABRT, SIGABRT_BUFFER );
+    install( SIGFPE, SIGFPE_BUFFER );
+
+    reinitSignalHandlers();
 
     std::set_terminate([]() {
-        static char buffer[1024];
+        static char buffer[ 1024 ];
 
         auto type = abi::__cxa_current_exception_type();
-        if (!type)
-        {
+        if ( not type ) {
             // 3 means to remove 3 stack frames: this way the backtrace starts at std::terminate
-            writeCrashLog("terminate was called although no exception was thrown", 3);
+            writeCrashLog( "terminate() was called although no exception was thrown", 3 );
+            releaseCrashLogger();
+            exit( 0 );
         }
 
-        const char *typeName = type->name();
-        if (typeName)
-        {
+        const char* typeName = type->name();
+        if ( typeName ) {
             int status;
-            abi::__cxa_demangle(typeName, demangleBuffer, &demangleBufferSize, &status);
-            if (status == 0 && *demangleBuffer)
-            {
+            abi::__cxa_demangle( typeName, demangleBuffer, &demangleBufferSize, &status );
+            if ( status == 0 and *demangleBuffer ) {
                 typeName = demangleBuffer;
             }
         }
-        try
-        {
+
+        try {
             throw;
-        }
-        catch (const std::exception &exc)
-        {
-            snprintf(buffer, sizeof(buffer), "uncaught exception of type %s (%s)",
-                     typeName, exc.what());
-        }
-        catch (...)
-        {
-            snprintf(buffer, sizeof(buffer), "uncaught exception of type %s", typeName);
+        } catch ( const std::exception &exc ) {
+            snprintf( buffer, sizeof( buffer ), "uncaught exception of type %s (%s)",
+                     typeName, exc.what() );
+        } catch (...) {
+            snprintf( buffer, sizeof( buffer ), "uncaught exception of type %s", typeName );
         }
 
         // 4 means to remove 4 stack frames: this way the backtrace starts at std::terminate
-        writeCrashLog(buffer, 4);
+        writeCrashLog( buffer, 4 );
+        releaseCrashLogger();
+        exit( 0 );
     });
 }
 
-static void writeCrashLog(const char *why, int stackFramesToIgnore)
-{
-    pid_t pid = getpid();
-    char who[256];
-    int whoLen = readlink("/proc/self/exe", who, sizeof(who) - 1);
-    who[qMax(0, whoLen)] = '\0';
+static void writeCrashLog( const char* why, const int stackFramesToIgnore ) {
+    printCurrentTime();
+    printToLog( "%s\n", why );
 
-    fprintf(stderr, "\n*** process %s (%d) crashed ***\n\n > why: %s\n", who, pid, why);
-
-    if (printBacktrace)
+    static auto printBacktraceLine = []( int level,
+                                         const char* symbol,
+                                         uintptr_t offset,
+                                         const char* file = nullptr,
+                                         int line = -1)
     {
-#ifdef BACKTRACE_SUPPORTED
-        struct btData
-        {
-            backtrace_state *state;
-            int level;
-        };
+        printToLog( " %3d: ", level );
 
-        static auto printBacktraceLine = [](int level, const char *symbol, uintptr_t offset, const char *file = nullptr,
-                                            int line = -1) {
-            const char *fmt1 = " %3d: %s [%" PRIxPTR "]";
-            const char *fmt2 = " in %s:%d";
-            if (useAnsiColor)
-            {
-                fmt1 = " %3d: \x1b[1m%s\x1b[0m [\x1b[36m%" PRIxPTR "\x1b[0m]";
-                fmt2 = " in \x1b[35m%s\x1b[0m:\x1b[35;1m%d\x1b[0m";
-            }
+        if ( not symbol ) {
+            printToLog( "--there is no valid data--\n" );
+            return;
+        }
+        printToLog( "%s [%" PRIxPTR "]", symbol, offset );
 
-            fprintf(stderr, fmt1, level, symbol, offset);
-            if (file)
-                fprintf(stderr, fmt2, file, line);
-            fputs("\n", stderr);
-        };
+        if ( not file ) {
+            printToLog( "\n" );
+            return;
+        }
 
-        static auto errorCallback = [](void *data, const char *msg, int errnum) {
-            const char *fmt = " %3d: ERROR: %s (%d)\n";
-            if (useAnsiColor)
-                fmt = " %3d: \x1b[31;1mERROR: \x1b[0;1m%s (%d)\x1b[0m\n";
+        printToLog( " in %s:%d\n", file, line );
+    };
 
-            fprintf(stderr, fmt, static_cast<btData *>(data)->level, msg, errnum);
-        };
+    struct btData {
+        backtrace_state *state;
+        int level;
+    };
 
-        static auto syminfoCallback = [](void *data, uintptr_t pc, const char *symname, uintptr_t symval,
-                                         uintptr_t symsize) {
-            Q_UNUSED(symval)
-            Q_UNUSED(symsize)
-
-            int level = static_cast<btData *>(data)->level;
-            if (symname)
-            {
-                int status;
-                abi::__cxa_demangle(symname, demangleBuffer, &demangleBufferSize, &status);
-
-                if (status == 0 && *demangleBuffer)
-                    printBacktraceLine(level, demangleBuffer, pc);
-                else
-                    printBacktraceLine(level, symname, pc);
-            }
-            else
-            {
-                printBacktraceLine(level, nullptr, pc);
-            }
-        };
-
-        static auto fullCallback = [](void *data, uintptr_t pc, const char *filename, int lineno,
-                                      const char *function) -> int {
-            if (function)
-            {
-                int status;
-                abi::__cxa_demangle(function, demangleBuffer, &demangleBufferSize, &status);
-
-                printBacktraceLine(static_cast<btData *>(data)->level,
-                                   (status == 0 && *demangleBuffer) ? demangleBuffer : function, pc,
-                                   filename ? filename : "<unknown>", lineno);
-            }
-            else
-            {
-                backtrace_syminfo(static_cast<btData *>(data)->state, pc, syminfoCallback, errorCallback, data);
-            }
-            return 0;
-        };
-
-        static auto simpleCallback = [](void *data, uintptr_t pc) -> int {
-            if (static_cast<btData *>(data)->level > stackFramesLimit)
-                return 1;
-
-            backtrace_pcinfo(static_cast<btData *>(data)->state, pc, fullCallback, errorCallback, data);
-            static_cast<btData *>(data)->level++;
-            return 0;
-        };
-
-        struct backtrace_state *state =
-            backtrace_create_state(nullptr, BACKTRACE_SUPPORTS_THREADS, errorCallback, nullptr);
-
-        fprintf(stderr, "\n > backtrace:\n");
-        btData data = {state, 0};
-        // backtrace_print(state, stackFramesToIgnore, stderr);
-        backtrace_simple(state, stackFramesToIgnore, simpleCallback, errorCallback, &data);
-#endif  // defined(BACKTRACE_SUPPORTED)
-    }
-    if (dumpCore)
+    static auto errorCallback = []( void *data,
+                                    const char *msg,
+                                    int errnum )
     {
-        fprintf(stderr, "\n > the process will be aborted (core dump)\n\n");
+        if ( data && msg ) {
+            auto bt = static_cast< btData* >( data );
+            printToLog( " %3d: ERROR: %s (%d)\n", bt->level, msg, errnum );
+        } else {
+            printToLog( " --level-not-defined--: ERROR: --not-defined-- (%d)\n", errnum );
+        }
+    };
 
-        //
-        //{ SIGFPE, SIGSEGV, SIGILL, SIGBUS, SIGPIPE, SIGABRT };
-        signal(SIGSEGV, SIG_DFL);
-        signal(SIGABRT, SIG_DFL);
-        signal(SIGFPE, SIG_DFL);
-        //
-        abort();
-    }
-    _exit(-1);
+    static auto syminfoCallback = []( void *data,
+                                      uintptr_t pc,
+                                      const char *symname,
+                                      uintptr_t,
+                                      uintptr_t )
+    {
+        if ( not data ) {
+            printToLog( "Internal error - an invalid 'data' for syminfoCallback." );
+            return;
+        }
+
+        int level = static_cast< btData* >( data )->level;
+        if ( not symname ) {
+            printBacktraceLine( level, nullptr, pc);
+            return;
+        }
+
+        int status;
+        abi::__cxa_demangle(symname, demangleBuffer, &demangleBufferSize, &status);
+
+        if (status == 0 && *demangleBuffer) {
+            printBacktraceLine(level, demangleBuffer, pc);
+            return;
+        }
+
+        printBacktraceLine(level, symname, pc);
+    };
+
+    static auto fullCallback = []( void *data,
+                                   uintptr_t pc,
+                                   const char *filename,
+                                   int lineno,
+                                   const char *function) -> int
+    {
+        if ( not data ) {
+            printToLog( "Internal error - an invalid 'data' for fullCallback.\n" );
+            return 0;
+        }
+
+        auto bt = static_cast< btData* >( data );
+
+        if ( not function ) {
+            backtrace_syminfo( bt->state, pc, syminfoCallback, errorCallback, data );
+            return 0;
+        }
+
+        int status;
+        abi::__cxa_demangle(function, demangleBuffer, &demangleBufferSize, &status);
+
+        printBacktraceLine( bt->level,
+                           (status == 0 && *demangleBuffer) ? demangleBuffer : function, pc,
+                           filename ? filename : "<unknown>", lineno);
+
+        return 0;
+    };
+
+    static auto simpleCallback = []( void *data, uintptr_t pc ) -> int {
+        if ( not data ) {
+            printToLog( "Internal error - an invalid 'data' for simpleCallback.\n" );
+            return 0;
+        }
+
+        auto bt = static_cast< btData* >( data );
+
+        const int stack_frames_limit = 100;
+        if ( bt->level > stack_frames_limit ) {
+            return 1;
+        }
+
+        backtrace_pcinfo( bt->state, pc, fullCallback, errorCallback, data);
+        bt->level++;
+        return 0;
+    };
+
+    auto state = backtrace_create_state( nullptr, BACKTRACE_SUPPORTS_THREADS, errorCallback, nullptr );
+    printToLog( "\n > backtrace:\n" );
+    btData data = { state, 0 };
+    backtrace_simple( state, stackFramesToIgnore, simpleCallback, errorCallback, &data );
 }
